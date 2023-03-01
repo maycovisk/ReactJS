35c18ac5303f7789d6266d6565bb4f0493bfb384
Maycon Lima <90663036+maycovisk@users.noreply.github.com>
2023-03-01 16:41:37 -0300

    Add files via upload

--- Projeto_Costs/src/components/layout/Footer.js
+++ Projeto_Costs/src/components/layout/Footer.js
@@ -5,9 +5,8 @@ import styles from './Footer.module.css'
 function Footer(){
     return (<footer className={styles.footer}>
         <ul className={styles.social_list}>
-            <li><FaGithub /></li>
-            <li><FaInstagram /></li>
-            <li><FaLinkedin /></li>
+            <li><a href="https://github.com/maycovisk" target="_blank"><FaGithub /></a></li>
+            <li><a href="https://www.linkedin.com/in/mayconlimac" target="_blank"><FaLinkedin /></a></li>
         </ul>
         <p className={styles.copy_right}>
             <span>Costs</span>&copy; 2023
